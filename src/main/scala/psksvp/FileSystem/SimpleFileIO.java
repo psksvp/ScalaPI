4dd7d5cedee507141bed29f84711d9aae3d1d62b
Pongsak Suvanpong <psksvp@gmail.com>
2015-12-11 16:04:34 +1100

    remove unnecessary code.

--- src/main/scala/psksvp/FileSystem/SimpleFileIO.java
+++ src/main/scala/psksvp/FileSystem/SimpleFileIO.java
@@ -547,47 +547,7 @@ public class SimpleFileIO
             in.close();
         }
     }
-
-    /**
-     * code is taken from
-     * http://fahdshariff.blogspot.jp/2011/08/changing-java-library-path-at-runtime.html
-     * @param path
-     * @throws Exception
-     */
-    public static void setLibraryPath(String path) throws Exception
-    {
-        System.setProperty("java.library.path", path);
-        //set sys_paths to null
-        final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
-        sysPathsField.setAccessible(true);
-        sysPathsField.set(null, null);
-    }
-
-    /**
-     * code is taken from
-     * http://fahdshariff.blogspot.jp/2011/08/changing-java-library-path-at-runtime.html
-     * @param pathToAdd
-     * @throws Exception
-     */
-    public static void addLibraryPath(String pathToAdd) throws Exception{
-        final Field usrPathsField = ClassLoader.class.getDeclaredField("usr_paths");
-        usrPathsField.setAccessible(true);
-
-        //get array of paths
-        final String[] paths = (String[])usrPathsField.get(null);
-
-        //check if the path to add is already present
-        for(String path : paths) {
-            if(path.equals(pathToAdd)) {
-                return;
-            }
-        }
-
-        //add the new path
-        final String[] newPaths = Arrays.copyOf(paths, paths.length + 1);
-        newPaths[newPaths.length-1] = pathToAdd;
-        usrPathsField.set(null, newPaths);
-    }
+    
 
     /**
      * code is taken from https://github.com/adamheinrich/native-utils
