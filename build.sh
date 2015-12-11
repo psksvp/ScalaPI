98b7899c85fad180f2708caa49da360443d711e2
Pongsak Suvanpong <psksvp@gmail.com>
2015-12-11 11:30:31 +1100

    ""

--- build.sh
+++ build.sh
@@ -12,5 +12,17 @@ ln -s ../JNI/SenseHatIMU/libPiSensors.so .
 cd ..
 cp JNI/SenseHatIMU/*.so .
 cp JNI/i2c/*.so .
-sbt compile
-sbt run
+sbt package
+mkdir temp
+cd temp
+unzip ../target/scala-2.11/scalapi_2.11-0.1.jar
+unzip -n ../lib/PiSensors.jar
+unzip -n ../lib/PiI2C.jar
+mkdir native
+cd native
+cp ../JNI/i2c/libPiI2C.so .
+cp ../JNI/SenseHatIMU/libPiSensors.so .
+cd ../..
+jar cvfm ScalaPi.jar MANIFEST.MF temp/ .
+
+
