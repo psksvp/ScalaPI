9cff47560e6f6633e7812aa6e8025aa2b5564581
Pongsak Suvanpong <psksvp@gmail.com>
2016-09-25 12:41:00 +1000

    fix getScala.sh to 2.11.8

--- getScala.sh
+++ getScala.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl https://dl.bintray.com/sbt/debian/sbt-0.13.9.deb > sbt-0.13.9.deb
-curl http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.deb?_ga=1.154127736.1386774362.1448663611 > scala-2.11.7.deb
+curl http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.deb
 sudo dpkg -i sbt-0.13.9.deb 
-sudo dpkg -i scala-2.11.7.deb
+sudo dpkg -i scala-2.11.8.deb
